1034a2b89eaed2dfc4df4f25137b51b2a60b7952
Stanislav Budinov <budinov.stanislav@gmail.com>
2022-01-20 14:50:57 +0200

    Google cloud cluster configuration.

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  identityApiUrl: 'http://localhost:5000/',
-  surveyApiUrl: 'http://localhost:5002/',
-  companyApiUrl: 'http://localhost:5004/',
-  notificationsUrl: 'http://localhost:5006/'
+  identityApiUrl: 'http://34.66.90.12:5000/',
+  surveyApiUrl: 'http://34.136.128.112:5002/',
+  companyApiUrl: 'http://34.135.152.93:5004/',
+  notificationsUrl: 'http://35.223.106.46:5006/',
+  statisticsApiUrl: '',
+  dealersApiUrl: '',
 };
